00835abe0a12adcb5493d2dd41609a1a4efcb3a8
AungKhant <aung.khant@ucsy.edu.mm>
2022-02-20 16:24:17 +0630

    project preparation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string("photo")->default("default-user-photo.png");
             $table->rememberToken();
             $table->timestamps();
         });
