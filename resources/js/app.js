b52f3d12929a18b0485a891f6c956e13a976aece
AungKhant <aung.khant@ucsy.edu.mm>
2022-03-17 06:23:46 +0630

    adding scrollreveal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,12 @@
 require('./bootstrap');
+import ScrollReveal from 'scrollreveal';
 
+ScrollReveal().reveal('.post', {
+    origin: 'top',
+    distance: '30px',
+    duration: 500,
+    interval: 500,
+})
 
 new VenoBox({
     selector: '.venobox'
